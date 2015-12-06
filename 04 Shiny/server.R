df37404a0da1376661436870762a79c52026a5c2
unknown <wes.james323@gmail.com>
2015-12-06 11:52:12 -0600

    Got the barchart pretty good but can't check until the crosstab is fixed and I need your help with that at this point.

--- 04 Shiny/server.R
+++ 04 Shiny/server.R
@@ -11,18 +11,18 @@ require(DT)
 shinyServer(function(input, output) {
   
   dfct <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from COLLEGESTATS"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
+  
   KPI_Low_Max_value <- reactive({input$KPI1})     
   KPI_Medium_Max_value <- reactive({input$KPI2})
   
   
   output$distPlot1 <- renderPlot(height=600, width=800, {
     
-    dfct2 <- dfct %>% mutate(kpi = ifelse(GRADUATIONRATE <= KPI_Low_Max_value(), '03 Low', ifelse(GRADUATIONRATE <= KPI_Medium_Max_value(), '02 Medium', '01 High')))
+    dfct2 <- dfct %>% mutate(kpi = ifelse(GRADUATIONRATE <= KPI_Low_Max_value(), '03 Low', ifelse(GRADUATIONRATE <= KPI_Medium_Max_value(), '02 Medium', '01 High'))) #%>% View()
     
     crosstab <- eventReactive(c(input$clicks1), 
-                {crosstab <- dfct2() %>% group_by(PUBLICPRIVATE, kpi) %>% summarize(avg_tuition = AVG(TUITIONFEES1314)) 
+                {crosstab <- dfct2 %>% group_by(PUBLICPRIVATE, kpi) %>% summarize(avg_tuition = mean(TUITIONFEES1314)) 
                 })
-    
     
     output$distPlot1 <- renderPlot(height=600, width=800, {
       plot <- ggplot() + 
@@ -31,7 +31,7 @@ shinyServer(function(input, output) {
       scale_y_discrete() +
       labs(title='College Statitics Graduation Rank Percentile \n By Tuition') +
       labs(x=paste("Public/Private"), y=paste("KPI")) +
-      layer(data=crosstab, 
+      layer(data=crosstab(), 
             mapping=aes(x=PUBLICPRIVATE, y=kpi, label=""), 
             stat="identity", 
             stat_params=list(), 
@@ -39,7 +39,7 @@ shinyServer(function(input, output) {
             geom_params=list(colour="black"), 
             position=position_identity()
       ) +
-      layer(data=crosstab, 
+      layer(data=crosstab(), 
             mapping=aes(x=PUBLICPRIVATE, y=kpi, label=""), 
             stat="identity", 
             stat_params=list(), 
@@ -47,7 +47,7 @@ shinyServer(function(input, output) {
             geom_params=list(colour="black"), 
             position=position_identity()
       ) +
-      layer(data=crosstab, 
+      layer(data=crosstab(), 
             mapping=aes(x=PUBLICPRIVATE, y=kpi, label=avg_tuition), 
             stat="identity", 
             stat_params=list(), 
@@ -63,13 +63,10 @@ shinyServer(function(input, output) {
   })
   # Begin code for Second Tab, Bar Chart:
   
-  df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from COLLEGESTATS"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), )) 
+  df <- eventReactive(input$clicks2, { 
+    df <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from COLLEGESTATS"'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), )) })
   
-  df2 <- df %>% mutate(AVG_DIFFERENCE = (AVERAGETOTALPAYMENTS - AVERAGEMEDICAREPAYMENTS), AVG_DIFF = mean(AVG_DIFFERENCE)) %>% group_by(DRGDEFINITION, AVERAGETOTALPAYMENTS, AVERAGEMEDICAREPAYMENTS, AVG_DIFFERENCE) %>% summarize(AVG_DIFFER = mean(AVG_DIFFERENCE)) 
-  
-  df3 <- df2 %>% ungroup %>% group_by(DRGDEFINITION) %>% summarise(AVG_DIFF = mean(AVG_DIFFER))
-  
-  df4 <- eventReactive(input$clicks2, {inner_join(df2, df3, by="DRGDEFINITION") })
+  df2 <- df() %>% mutate(avg_pell = mean(AVGAMTFIRSTTIMEUGPELL), avg_state = mean(AVGAMTFIRSTTIMEUGSTATEGRANT), avg_fed = mean(AVGAMTFIRSTTIMEUGOTHFED), avg_ins = mean(AVGAMTFIRSTTIMEUGINSGRANT))
   
   output$distPlot2 <- renderPlot(height=600, width=800, {
     plot1 <- ggplot() + 
@@ -78,10 +75,10 @@ shinyServer(function(input, output) {
       #scale_x_continuous() +
       scale_y_continuous() +
       facet_wrap(~DRGDEFINITION, ncol=1) +
-      labs(title='Medical Data \n Procedure Cost Comparison ') +
-      labs(x=paste("Average Price"), y=paste("Measure Names")) +
+      labs(title='Public private cost Comparison ') +
+      labs(x=paste("I'm not sure"), y=paste("Measure Names")) +
       layer(data=df4(), 
-            mapping=aes(x=paste("AVERAGETOTALPAYMENTS"), y=AVERAGETOTALPAYMENTS), 
+            mapping=aes(x=paste("AVGAMTFIRSTTIMEUGPELL"), y=avg_pell), 
             stat="identity", 
             stat_params=list(), 
             geom="bar",
@@ -89,7 +86,7 @@ shinyServer(function(input, output) {
             position=position_identity()
       ) + coord_flip() +
       layer(data=df4(), 
-            mapping=aes(x=paste("AVERAGEMEDICAREPAYMENTS"), y=AVERAGEMEDICAREPAYMENTS), 
+            mapping=aes(x=paste("AVGAMTFIRSTTIMEUGSTATEGRANT"), y=avg_state), 
             stat="identity", 
             stat_params=list(), 
             geom="bar",
@@ -97,7 +94,7 @@ shinyServer(function(input, output) {
             position=position_identity()
       ) +
       layer(data=df4(), 
-            mapping=aes(x=paste("AVG_DIFF"), y=AVG_DIFF), 
+            mapping=aes(x=paste("AVGAMTFIRSTTIMEUGOTHFED"), y=avg_fed), 
             stat="identity", 
             stat_params=list(), 
             geom="bar",
@@ -105,7 +102,15 @@ shinyServer(function(input, output) {
             position=position_identity()
       ) +
       layer(data=df4(), 
-            mapping=aes(x=paste("AVG_DIFF"), y=AVG_DIFF, label=round(AVG_DIFF)), 
+            mapping=aes(x=paste("AVGAMTFIRSTTIMEUGINSGRANT"), y=avg_ins), 
+            stat="identity", 
+            stat_params=list(), 
+            geom="bar",
+            geom_params=list(colour="yellow"), 
+            position=position_identity()
+      ) +
+      layer(data=df4(), 
+            mapping=aes(x=paste("AVGAMTFIRSTTIMEUGINSGRANT"), y=avg_ins, label=round(avg_ins)), 
             stat="identity", 
             stat_params=list(), 
             geom="text",
